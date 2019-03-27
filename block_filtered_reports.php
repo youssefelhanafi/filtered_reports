ae2a5ca0ce2209d604a300f92ff90a9c4de3a824
youssefelhanafi <root@localhost.localdomain>
2019-03-27 10:35:48 +0100

    Init commit 001

---
+++ block_filtered_reports.php
@@ -0,0 +1,8 @@
+<?php
+class block_filtered_reports extends block_base {
+    public function init() {
+        $this->title = get_string('filtered_reports', 'block_filtered_reports');
+    }
+    // The PHP tag and the curly bracket for the class definition 
+    // will only be closed after there is another function added in the next section.
+}
